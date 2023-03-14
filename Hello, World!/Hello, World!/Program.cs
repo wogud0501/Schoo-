a10360badf2b3dc22968963859e23750e667441d
a <djaeyeo@gmail.com>
2023-03-14 16:20:06 +0900

    A

--- Hello, World!/Hello, World!/Program.cs
+++ Hello, World!/Hello, World!/Program.cs
@@ -10,12 +10,26 @@ namespace Hello__World_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Typing Your Name : ");
-            string a = Console.ReadLine();
-            Console.WriteLine("my name is :" + a);
-            Console.WriteLine("Typing User Age : ");
-            int b = int.Parse(Console.ReadLine());
-            Console.WriteLine("I am " + b + " Years Old.");    
+            Console.Write("삼각형의 높이를 입력하세요 : ");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= height; i++)
+            {
+                
+                for (int j = 1; j <= height - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+               
+                for (int k = 1; k <= i * 2 - 1; k++)
+                {
+                    Console.Write("*");
+                }
+
+                
+                Console.WriteLine();
+            }
         }
     }
 }
