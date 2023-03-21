eb41505f5d8d579c0bb9418c1b154d7d4228b11c
a <djaeyeo@gmail.com>
2023-03-21 14:30:21 +0900

    a

--- Hello, World!/Hello, World!/Program.cs
+++ Hello, World!/Hello, World!/Program.cs
@@ -10,26 +10,37 @@ namespace Hello__World_
     {
         static void Main(string[] args)
         {
-            Console.Write("삼각형의 높이를 입력하세요 : ");
-            int height = Convert.ToInt32(Console.ReadLine());
-
-            for (int i = 1; i <= height; i++)
+            Console.WriteLine("점수 입력");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("과목 1 점수 : " + a);
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("과목 2 점수 : " + b);
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine("과목 3 점수 : " + c);
+            int d = int.Parse(Console.ReadLine());
+            Console.WriteLine("과목 4 점수 : " + d);
+            int e = a + b + c + d;
+            Console.WriteLine("4과목 점수 총합 : " + e);
+            int f = e / 4;
+            Console.WriteLine("4과목 점수 평균 : " + f);
+            if (f >= 90)
             {
-                
-                for (int j = 1; j <= height - i; j++)
-                {
-                    Console.Write(" ");
-                }
-
-               
-                for (int k = 1; k <= i * 2 - 1; k++)
-                {
-                    Console.Write("*");
-                }
-
-                
-                Console.WriteLine();
+                Console.WriteLine("등급 : A");
             }
+            else if (f >= 80)
+            {
+                Console.WriteLine("등급 : B");
+            }
+            else if (f >= 70)
+            {
+                Console.WriteLine("등급 : C");
+            }
+            else if (f >= 60)
+            {
+                Console.WriteLine("등급 : D");
+            }
+            else if (f < 60)
+                Console.WriteLine("등급 : E");
         }
     }
 }
