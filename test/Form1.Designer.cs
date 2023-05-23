0c079fa8494fd77d12766b2841dd8e19b8445bd6
a <djaeyeo@gmail.com>
2023-05-23 16:10:39 +0900

    A

--- test/Form1.Designer.cs
+++ test/Form1.Designer.cs
@@ -29,13 +29,96 @@ namespace Test
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.progressBar);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(688, 357);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(224, 20);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(225, 12);
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(464, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "검색";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(161, 151);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(297, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(268, 209);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Test.Properties.Resources.다운로드;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(211, 48);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(247, 97);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(711, 383);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "표 만들기";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- test/Form1.cs
+++ test/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,63 @@ namespace Test
 {
     public partial class Form1 : Form
     {
+        private bool isProgressBarVisible = false;
         public Form1()
         {
             InitializeComponent();
+            progressBar.Visible = false;
         }
-    }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (isProgressBarVisible)
+            {
+                // 프로그레스바 초기화 및 숨김 처리
+                progressBar.Value = 0;
+                progressBar.Visible = false;
+                isProgressBarVisible = false;
+            }
+            else
+            {
+                // 프로그레스바 표시
+                progressBar.Visible = true;
+                isProgressBarVisible = true;
+
+                // 프로그레스바 값 증가를 위한 타이머 설정
+                Timer timer = new Timer();
+                timer.Interval = 100;  // 타이머 간격 (ms)
+                timer.Tick += Timer_Tick;  // 타이머 이벤트 핸들러 등록
+                timer.Start();  // 타이머 시작
+            }
+            using (StreamWriter writer = new StreamWriter("test.txt", true))
+            {
+                writer.WriteLine(label1.Text);
+            }
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            progressBar.Value += 10;
+
+            if (progressBar.Value >= progressBar.Maximum)
+            {
+                ((Timer)sender).Stop();
+                MessageBox.Show("검색이 완료 되었습니다.");
+                progressBar.Visible = false;
+
+                Random random = new Random();
+                string link = "https://google.com/" + random.Next(1000, 9999);
+
+                label1.Text = link;
+            }
+
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }               
 }
